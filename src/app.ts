0330965bf8f270d21976c53a036baa0cb7716853
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2024-06-17 00:19:13 +0600

    Project setup and initialization done

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/', router);
+
+export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+const routes = [
+    
+];
+
+export default router;
