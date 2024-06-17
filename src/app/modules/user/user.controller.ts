b884972db6a19577a687dc4112701dda6570a60a
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2024-06-17 12:32:14 +0600

    Addition: Created route for user creation

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { userServices } from './user.service';
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = req.body;
+    const result = await userServices.createUser(user);
+
+    res.status(200).json({
+      success: true,
+      message: 'User created successfully!',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong!',
+      error: err,
+    });
+  }
+};
+export const UserControllers = {
+  createUser,
+};
