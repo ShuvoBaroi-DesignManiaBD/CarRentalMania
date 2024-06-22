d3974f40385fcfb77ff603c90fb18b41e765671f
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2024-06-22 09:18:23 +0600

    Addition: Created interface, mongoose-schema & model for car

---
+++ src/app/modules/car/car.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema} from 'mongoose';
+import { TCar } from './car.interface';
+
+
+// the car schema
+const carSchema = new Schema<TCar>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  color: { type: String, required: true },
+  isElectric: { type: Boolean, required: true },
+  status: { type: String, enum: ['available', 'unavailable'], default: 'available' },
+  features: { type: [String], required: true },
+  pricePerHour: { type: Number, required: true },
+  isDeleted: { type: Boolean, default: false },
+});
+
+// the car model
+export const Car = mongoose.model<TCar>('Car', carSchema);
