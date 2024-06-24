42c11aaa877112f205a59a1051962711779038f1
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2024-06-24 11:35:25 +0600

    Improvement: Added custom function for handeling duplication errors

---
+++ src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
+
+const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  const match = err.message.match(/"([^"]*)"/);
+
+  const matchedMessage = match && match[1];
+
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: `${matchedMessage} is already exists`,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Invalid ID',
+    errorSources,
+  };
+};
+
+export default handleDuplicateError;
